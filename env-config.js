7edac6f41d783de6e0b61a9ae0119218fa6bdef6
Muhammad Wyzer <mwyzer@gmail.com>
2021-02-10 07:24:59 +0700

    Error date.clone is not a function

--- env-config.js
+++ env-config.js
@@ -1,7 +1,7 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  'process.env.BASE_URL': prod ? 'https://filipjerga.herokuapp.com' : 'http://localhost:3000',
-  'process.env.NAMESPACE': 'https://filipjerga.herokuapp.com',
-  'process.env.CLIENT_ID': 'NfvS9nw81ItncHJKPHCaAvwD9ChNWYn3'
+  'process.env.BASE_URL': prod ? 'https://portfolio-wyzer.herokuapp.com' : 'http://localhost:3000',
+  'process.env.NAMESPACE': 'https://portfolio-wyzer.herokuapp.com',
+  'process.env.CLIENT_ID': 'ksCoEENrNdC0S5f9eG2evrJt3a39Z1NF'
 }
--- server/services/auth.js
+++ server/services/auth.js
@@ -10,10 +10,10 @@ exports.checkJWT = jwt({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 50,
-    jwksUri: 'https://eincode.eu.auth0.com/.well-known/jwks.json'
+    jwksUri: 'https://dev-vag0d-ut.au.auth0.com/.well-known/jwks.json'
   }),
-  audience: 'NfvS9nw81ItncHJKPHCaAvwD9ChNWYn3',
-  issuer: 'https://eincode.eu.auth0.com/',
+  audience: 'ksCoEENrNdC0S5f9eG2evrJt3a39Z1NF',
+  issuer: 'https://dev-vag0d-ut.au.auth0.com/',
   algorithms: ['RS256']
 })
 
